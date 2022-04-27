251db673c39a1d00d76a925fd9d31612e50c43f6
joel5vega <joel5vega@gmail.com>
2022-04-27 00:32:13 -0400

    fixced

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div>Desarrollado por <a href="https://joelvega.me">Joel Vega</a></div>
   )
 }
 
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -16,12 +16,7 @@ function Results(props) {
       {props.show && (
         <div>
           <h1>Resultados</h1>
-          {props.dones.map((don) => (
-            <div key={don.id}>
-              <h3>{don.name}:{don.score}</h3>
-            </div>
-          ))}
-          <ResponsiveContainer width="100%" height={300}>
+           <ResponsiveContainer width="100%" height={300}>
             <BarChart width={150} height={300} data={props.dones}>
               <CartesianGrid strokeDasharray="3 3" />
               <XAxis angle={270} dataKey="name" interval={0}/>
@@ -31,6 +26,12 @@ function Results(props) {
               <Bar dataKey="score" fill="#8884d8" label={{position:'top'}}/>
             </BarChart>
           </ResponsiveContainer>
+          {props.dones.map((don) => (
+            <div key={don.id}>
+              <h3>{don.name}:{don.score}</h3>
+            </div>
+          ))}
+         
         </div>
       )}
     </div>
