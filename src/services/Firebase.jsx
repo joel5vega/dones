6d46e33f7359c738fdd78c1e02d10fbcefdc0e70
Joel <joel5vega@gmail.com>
2023-08-03 10:29:16 -0400

    correct db connection

--- src/services/Firebase.jsx
+++ src/services/Firebase.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getDatabase,ref,set,onValue} from "firebase/database";
+import {getFirestore,doc,getDoc} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,15 +19,20 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-const connectedRef = firebase.database().ref('.info/connected');
-connectedRef.on('value', (snap) => {
-  if (snap.val() === true) {
-    console.log('Firebase is connected');
-  } else {
-    console.log('Firebase is not connected');
+async function verifyFirestoreConnection() {
+  try {
+    const docRef = doc(db, 'test', 'test');
+    const docSnap = await getDoc(docRef);
+    console.log('Firestore is connected');
+  } catch (e) {
+    console.error('Error connecting to Firestore:', e);
   }
-});
+}
+
+verifyFirestoreConnection();
+
 export default db;
