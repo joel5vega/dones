f2483c2c304cd2ef598c94ace0d804aaca59c42a
Joel <joel5vega@gmail.com>
2023-08-02 20:57:52 -0400

    add login page

--- src/services/GoogleSigIn.jsx
+++ src/services/GoogleSigIn.jsx
@@ -1,4 +1,5 @@
-import firebase from 'firebase/app';
+// import firebase from 'firebase/app';
+import {getAuth,GoogleAuthProvider,signInWithPopup}from 'firebase/auth';
 const provider = new firebase.auth.GoogleAuthProvider();
 
 firebase.auth().signInWithPopup(provider)
