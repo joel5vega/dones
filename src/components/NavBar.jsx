ae6cd95b5226c53599f1959b7cdc697e032d8b07
Joel <joel5vega@gmail.com>
2023-08-02 21:53:49 -0400

    add login to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,7 +30,8 @@ export default function ImageAvatars(props) {
           <Avatar alt="dones" src={DonesEspirituales} />
         </Link>
         <h2>{props.title}</h2>
-        <Avatar alt="user" src={<LoginPage/>} />
+       <LoginPage/>
+
         {!props.result ? (
           <Popover
             title="?"
