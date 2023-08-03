a9c7d49ef5e5d73f9f069b146ae94547e007e823
Joel <joel5vega@gmail.com>
2023-08-02 22:55:56 -0400

    correct icon

--- src/components/auth/SaveProgressButton.jsx
+++ src/components/auth/SaveProgressButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSave } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import Button from '@mui/material/Button';
 const SaveProgressButton = ({ onClick }) => {
   return (
